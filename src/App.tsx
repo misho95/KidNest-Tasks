ed568e32e7fff792286744f1df5db67544aa8bbb
misho95 <chapidze.misho95@gmail.com>
2024-01-24 14:36:01 +0400

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import HeaderDesktop from "./components/header.desktop";
+import HeaderMobile from "./components/header.mobile";
 import useDeviceDetection from "./utils";
 
 function App() {
   const device = useDeviceDetection();
 
-  return <>{device !== "Mobile" ? <HeaderDesktop /> : "mobile"}</>;
+  return <>{device !== "Mobile" ? <HeaderDesktop /> : <HeaderMobile />}</>;
 }
 
 export default App;
--- src/components/header.mobile.tsx
+++ src/components/header.mobile.tsx
@@ -1,5 +1,15 @@
+import menu from "../assets/ic-menu-item.png";
+import logo from "../assets/logo.png";
+import geo from "../assets/Flag_of_Georgia 1.png";
+
 const HeaderMobile = () => {
-  return <header></header>;
+  return (
+    <header>
+      <img src={menu} />
+      <img src={logo} />
+      <img src={geo} />
+    </header>
+  );
 };
 
 export default HeaderMobile;
