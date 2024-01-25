e200e2b5d5b9acfa7d35ca6bde9383573ff0a335
misho95 <chapidze.misho95@gmail.com>
2024-01-25 15:19:50 +0400

    fxd

--- src/components/custom.date.picker.tsx
+++ src/components/custom.date.picker.tsx
@@ -1,10 +1,17 @@
+import calendar from "../assets/ic-solar_calendar-mark-bold-duotone.png";
+
 const CustomDatePicker = () => {
   return (
-    <input
-      type="date"
-      placeholder="დაწყება"
-      className="w-[324px] h-[54px] border-[1px] border-[#919EAB33] rounded-[8px] py-[16px] px-[14px] text-[#919EAB] focus:outline-none"
-    />
+    <div className="relative">
+      <input
+        type="text"
+        placeholder="დაწყება"
+        className="w-[324px] h-[54px] border-[1px] border-[#919EAB33] rounded-[8px] py-[16px] px-[14px] text-[#919EAB] focus:outline-none"
+      />
+      <button className="absolute right-[14px] top-1/2 -translate-y-1/2">
+        <img src={calendar} />
+      </button>
+    </div>
   );
 };
 
